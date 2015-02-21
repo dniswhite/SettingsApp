f45213cb01c6d51009ad6a08e42c3ca3ac12e042
Dennis White <denniswhite98@yahoo.com>
2015-02-21 14:48:25 -0800

    initial
    initial work for new blog article

---
+++ SettingsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SettingsApp
+//
+//  Created by Dennis White on 2/21/15.
+//  Copyright (c) 2015 dniswhite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
